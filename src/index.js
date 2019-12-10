571d1e46148c8b3c05df00617ad35fe0caa60830
Mauro Meden <mauro.meden@gmail.com>
2019-12-09 20:32:49 -0500

    error handler moved to external file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 let express = require('express'); // Import express
 let router = require('./router'); // Import router
+let errorHandler = require('./errorHandler'); // Import error handler
 
 let app = express();
 
@@ -11,9 +12,8 @@ app.use(router); // Apply router as middleware
 
 // Default error handler
 app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-  })
+  errorHandler(err, res);
+});
 
 // for test purposes
 //app.use('/src/test_routes', require('./test_routes'));

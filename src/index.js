f884dac4b3617ac3d2723c65665d4ebaac617750
Mauro Meden <mauro.meden@gmail.com>
2019-12-10 06:30:10 -0500

    testing error handler

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@
 
 let express = require('express'); // Import express
 let router = require('./router'); // Import router
-let errorHandler = require('./errorHandler'); // Import error handler
+// let errorHandler = require('./errorHandler'); // Import error handler
 
 let app = express();
 
 // Apply middleware
-app.use(express.json());
+app.use(express.json()); // Allows to read and write json files easily
 app.use(router); // Apply router as middleware
 
 // Default error handler
-app.use(function (err, req, res, next) {
-  errorHandler(err, res);
+// To be used with imported error handler
+// app.use(function (error, req, res, next) {
+//   errorHandler(error, res);
+// });
+app.use(function (error, req, res, next) {
+  console.error(error); // We have this here so we can see any errors that happened in our console for debugging purposes
+  res.sendStatus(500); // We send a 500 status code to indicate there has been an "Internal Server Error"
 });
 
 // for test purposes

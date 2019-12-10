38dc145bc80b3e05836c242752d333129014c60a
Mauro Meden <mauro.meden@gmail.com>
2019-12-09 20:24:59 -0500

    error handler added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 'use strict';
 
-let express = require('express');
+let express = require('express'); // Import express
+let router = require('./router'); // Import router
+
 let app = express();
 
-app.use('/src/routes', require('./routes'));
+// Apply middleware
+app.use(express.json());
+app.use(router); // Apply router as middleware
+
+// Default error handler
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+  })
 
 // for test purposes
 //app.use('/src/test_routes', require('./test_routes'));
